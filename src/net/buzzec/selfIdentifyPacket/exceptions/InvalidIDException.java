8e225c1e616e08f391863b639b301658098fba10
Buzzec <bbetter9101@gmail.com>
2018-04-03 17:09:31 -0500

    Added Logging

--- src/net/buzzec/selfIdentifyPacket/exceptions/InvalidIDException.java
+++ src/net/buzzec/selfIdentifyPacket/exceptions/InvalidIDException.java
@@ -1,6 +1,8 @@
 package net.buzzec.selfIdentifyPacket.exceptions;
 
 import net.buzzec.selfIdentifyPacket.SelfIdentifyBaseIDs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Error indicating a custom SelfIdentifyObject ID is occupying the same value as a built-in ID
@@ -9,11 +11,15 @@ import net.buzzec.selfIdentifyPacket.SelfIdentifyBaseIDs;
  * @see SelfIdentifyBaseIDs
  */
 public class InvalidIDException extends RuntimeException{
+    private static Logger logger = LoggerFactory.getLogger(InvalidIDException.class);
+
     public InvalidIDException(byte id){
         super("Invalid ID: " + id);
+        logger.trace("Built InvalidIDException with byte id=" + id);
     }
 
     public InvalidIDException(short id){
         super("Invalid ID: " + id);
+        logger.trace("Built InvalidIDException with short id=" + id);
     }
 }
