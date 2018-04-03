bc1e5f56fd27946ef54cf1aecb7969262644645f
Buzzec <bbetter9101@gmail.com>
2018-04-03 17:12:15 -0500

    Added Logging

--- src/net/buzzec/selfIdentifyPacket/exceptions/RepliedPacketException.java
+++ src/net/buzzec/selfIdentifyPacket/exceptions/RepliedPacketException.java
@@ -3,6 +3,8 @@ package net.buzzec.selfIdentifyPacket.exceptions;
 import net.buzzec.selfIdentifyPacket.SelfIdentifyBasic;
 import net.buzzec.selfIdentifyPacket.SelfIdentifyPacketReader;
 import nl.pvdberg.pnet.packet.Packet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,7 +14,10 @@ import java.io.IOException;
  * @author Buzzec
  */
 public class RepliedPacketException extends RuntimeException{
+    private static Logger logger = LoggerFactory.getLogger(RepliedPacketException.class);
+
     public RepliedPacketException(Packet packet) throws IOException{
         super("Replied packet error: " + new SelfIdentifyPacketReader<>(packet, SelfIdentifyBasic.class).getInfoListFromPacket().get(0).object);
+        logger.trace("Built RepliedPacketException with packet.id=" + packet.getPacketID());
     }
 }
