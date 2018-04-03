53a8fa31e9898375b5fd8e2ec13a1c6b602dbcd9
Buzzec <bbetter9101@gmail.com>
2018-04-03 17:05:26 -0500

    Added JDocs

--- src/net/buzzec/selfIdentifyPacket/SelfIdentifyPacketRegistry.java
+++ src/net/buzzec/selfIdentifyPacket/SelfIdentifyPacketRegistry.java
@@ -16,7 +16,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-@SuppressWarnings("WeakerAccess")
+/**
+ * This class is used to register all of your {@code ISelfIdentifyEnum}s against a packet id so they are searchable when
+ * a packet is received.
+ *
+ * <p>
+ *     The same {@code ISelfIdentifyEnum} can be registered for different packet ids as the packet id is the key for the
+ *     map. This also means that different functions can be used for the same {@code ISelfIdentifyEnum}. When a packet
+ *     is received by a {@code SelfIdentifyListener}, it will search through the map of values. The functions can be
+ *     defended as null to show that they should not be run.
+ * </p>
+ * <p>
+ *     When an unknown packet is received the {@code onUnknownPacketServer} or {@code onUnknownPacketClient} are called
+ *     and can be set with {@code setOnUnknownPacketServer} and {@code setOnUnknownPacketClient}.
+ * </p>
+ */
+//TODO Add logging
+@SuppressWarnings({"WeakerAccess", "unused"})
 public class SelfIdentifyPacketRegistry{
     private static Map<Short, FunctionList> map = new HashMap<>();
     private static Function<Pair<Packet, Client>, Void> onUnknownPacketServer = null;
@@ -24,6 +40,16 @@ public class SelfIdentifyPacketRegistry{
 
     private static Logger logger = LoggerFactory.getLogger(SelfIdentifyPacketRegistry.class);
 
+    /**
+     * Registers a {@code ISelfIdentifyObjectEnum} under a packet id with functions that will run on the data received
+     * in that packet id.
+     *
+     * @param packetID The packet id to be registered under.
+     * @param clazz The {@code ISelfIdentifyObjectEnum} class to be registered.
+     * @param onReceiveServerAsync The function that will run if the packet is received on the server.
+     * @param onReceiveClientAsync The function that will run if the packet is received on the client.
+     * @return {@code true} if successful, {@code false} if not.
+     */
     public static <T extends Enum<T> & ISelfIdentifyObjectEnum> boolean registerPacketID(
             short packetID,
             @NotNull Class<T> clazz,
@@ -44,10 +70,19 @@ public class SelfIdentifyPacketRegistry{
         return true;
     }
 
+    /**
+     * Sets {@code onUnknownPacketServer} function.
+     *
+     * @param onUnknownPacketServer The function to be set to.
+     */
     public static void setOnUnknownPacketServer(Function<Pair<Packet, Client>, Void> onUnknownPacketServer){
         SelfIdentifyPacketRegistry.onUnknownPacketServer = onUnknownPacketServer;
     }
 
+    /**
+     * Sets {@code ononUnknownPacketClient} function.
+     * @param onUnknownPacketClient The function to be set to.
+     */
     public static void setOnUnknownPacketClient(Function<Pair<Packet, Client>, Void> onUnknownPacketClient){
         SelfIdentifyPacketRegistry.onUnknownPacketClient = onUnknownPacketClient;
     }
