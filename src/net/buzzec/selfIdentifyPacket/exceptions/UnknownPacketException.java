0c30dc4161214225150cb31be955c2723cbd6143
Buzzec <bbetter9101@gmail.com>
2018-04-03 17:16:21 -0500

    Added Logging

--- src/net/buzzec/selfIdentifyPacket/exceptions/UnknownPacketException.java
+++ src/net/buzzec/selfIdentifyPacket/exceptions/UnknownPacketException.java
@@ -5,6 +5,8 @@ import net.buzzec.selfIdentifyPacket.SelfIdentifyBasic;
 import net.buzzec.selfIdentifyPacket.SelfIdentifyPacketBuilder;
 import nl.pvdberg.pnet.client.Client;
 import nl.pvdberg.pnet.packet.Packet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 
@@ -14,11 +16,14 @@ import java.util.Arrays;
  * @author Buzzec
  */
 public class UnknownPacketException extends Exception{
+    private static Logger logger = LoggerFactory.getLogger(UnknownPacketException.class);
+
     public UnknownPacketException(Packet packet, Client client){
         super("Unknown packet id: " + packet.getPacketID());
         client.send(new SelfIdentifyPacketBuilder<>(Packet.PacketType.Reply, SelfIdentifyBasic.class, SelfIdentifyBaseIDs.PACKET_ERROR_ID)
             .withString(UnknownPacketException.class.getSimpleName() + ": " + Arrays.toString(getStackTrace()))
             .build()
         );
+        logger.trace("Built UnknownPacketException with packet.id=" + packet.getPacketID() + ", and client.InetAddress=" + client.getInetAddress());
     }
 }
