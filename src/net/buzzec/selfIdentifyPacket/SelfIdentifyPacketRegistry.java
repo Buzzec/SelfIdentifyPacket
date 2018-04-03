11c673bfbf2dad3b59d8047c9835accfe3106717
Buzzec <bbetter9101@gmail.com>
2018-04-03 16:44:16 -0500

    Made the map better.

--- src/net/buzzec/selfIdentifyPacket/SelfIdentifyPacketRegistry.java
+++ src/net/buzzec/selfIdentifyPacket/SelfIdentifyPacketRegistry.java
@@ -7,6 +7,8 @@ import nl.pvdberg.pnet.client.Client;
 import nl.pvdberg.pnet.packet.Packet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,7 +18,11 @@ import java.util.function.Function;
 
 @SuppressWarnings("WeakerAccess")
 public class SelfIdentifyPacketRegistry{
-    private static Map<Short, Pair<Class, Pair<Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void>, Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void>>>> map = new HashMap<>();
+    private static Map<Short, FunctionList> map = new HashMap<>();
+    private static Function<Pair<Packet, Client>, Void> onUnknownPacketServer = null;
+    private static Function<Pair<Packet, Client>, Void> onUnknownPacketClient = null;
+
+    private static Logger logger = LoggerFactory.getLogger(SelfIdentifyPacketRegistry.class);
 
     public static <T extends Enum<T> & ISelfIdentifyObjectEnum> boolean registerPacketID(
             short packetID,
@@ -30,41 +36,78 @@ public class SelfIdentifyPacketRegistry{
         if(map.containsKey(packetID)){
             return false;
         }
-        map.put(packetID, new Pair<>(clazz, new Pair<>(onReceiveServerAsync, onReceiveClientAsync)));
+        map.put(packetID, new FunctionList()
+                .clazz(clazz)
+                .onReceiveServerAsync(onReceiveServerAsync)
+                .onReceiveClientAsync(onReceiveClientAsync)
+        );
         return true;
     }
 
     static void applyServerFunction(Packet packet, Client client) throws IOException{
-        if(map.containsKey(packet.getPacketID())){
-            Class clazz = map.get(packet.getPacketID()).getKey();
-            Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> function = map.get(packet.getPacketID()).getValue().getKey();
-            if(function != null){
-                //noinspection unchecked
-                function.apply(new Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>(
-                        new SelfIdentifyPacketReader<>(packet, clazz).getInfoListFromPacket(),
-                        client
-                ));
+        try{
+            if(map.containsKey(packet.getPacketID())){
+                FunctionList list = map.get(packet.getPacketID());
+                if(list.onReceiveClientAsync != null){
+                    //noinspection unchecked
+                    list.onReceiveClientAsync.apply(new Pair<>(
+                            new SelfIdentifyPacketReader<>(packet, list.clazz).getInfoListFromPacket(),
+                            client
+                    ));
+                }
+            } else{
+                throw new UnknownPacketException(packet, client);
             }
         }
-        else{
-            throw new UnknownPacketException(packet, client);
+        catch(UnknownPacketException e){
+            logger.error("Unknown Packet: ", e);
+            if(onUnknownPacketServer != null){
+                onUnknownPacketServer.apply(new Pair<>(packet, client));
+            }
         }
     }
 
     static void applyClientFunction(Packet packet, Client client) throws IOException{
-        if(map.containsKey(packet.getPacketID())){
-            Class clazz = map.get(packet.getPacketID()).getKey();
-            Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> function = map.get(packet.getPacketID()).getValue().getValue();
-            if(function != null){
-                //noinspection unchecked
-                function.apply(new Pair<java.util.ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>(
-                        new SelfIdentifyPacketReader<>(packet, clazz).getInfoListFromPacket(),
-                        client
-                ));
+        try{
+            if(map.containsKey(packet.getPacketID())){
+                FunctionList list = map.get(packet.getPacketID());
+                if(list.onReceiveServerAsync != null){
+                    //noinspection unchecked
+                    list.onReceiveServerAsync.apply(new Pair<>(
+                            new SelfIdentifyPacketReader<>(packet, list.clazz).getInfoListFromPacket(),
+                            client
+                    ));
+                }
+            } else{
+                throw new UnknownPacketException(packet, client);
             }
         }
-        else{
-            throw new UnknownPacketException(packet, client);
+        catch(UnknownPacketException e){
+            logger.error("Unknown Packet: ", e);
+            if(onUnknownPacketClient != null){
+                onUnknownPacketClient.apply(new Pair<>(packet, client));
+            }
+        }
+    }
+
+    private static class FunctionList{
+        private Class clazz;
+        private Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> onReceiveServerAsync = null;
+        private Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> onReceiveClientAsync = null;
+
+        private FunctionList clazz(Class clazz){
+            this.clazz = clazz;
+            return this;
+        }
+
+        private FunctionList onReceiveServerAsync(@Nullable Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> onReceiveServerAsync){
+            this.onReceiveServerAsync = onReceiveServerAsync;
+            return this;
+        }
+
+        private FunctionList onReceiveClientAsync(@Nullable Function<Pair<ArrayList<SelfIdentifyPacketReader.ClassQualifiedObject>, Client>, Void> onReceiveClientAsync){
+            this.onReceiveClientAsync = onReceiveClientAsync;
+            return this;
         }
     }
 }
