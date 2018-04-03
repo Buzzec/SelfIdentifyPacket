aaa7ddbb837c57242bc064dbcc9ce2f3f38e7815
Buzzec <bbetter9101@gmail.com>
2018-04-03 17:13:23 -0500

    Added Logging

--- src/net/buzzec/selfIdentifyPacket/exceptions/UnknownObjectException.java
+++ src/net/buzzec/selfIdentifyPacket/exceptions/UnknownObjectException.java
@@ -1,12 +1,18 @@
 package net.buzzec.selfIdentifyPacket.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Error indicates when an Unknown object is found while reading a packet
  *
  * @author Buzzec
  */
 public class UnknownObjectException extends RuntimeException{
+    private static Logger logger = LoggerFactory.getLogger(UnknownObjectException.class);
+
     public UnknownObjectException(byte id){
         super("Unknown Object: " + id);
+        logger.trace("Built InvalidIDException with byte id=" + id);
     }
 }
